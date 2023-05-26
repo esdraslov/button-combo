70d7e38477b06747e4718e2263263a95105dacac
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-05-26 16:39:16 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-namespace combo{
-    
+namespace combo {
+    let combo: string
+    let combin: string
+    //% block
+    //% combo.min=1, combo.max=10
+    /**
+     * @param comb the combo, eg: AABAA
+     */
+    export function setCombo(...comb: string[]){
+        for(let i=0;i>comb.length;i++){
+            combo = combo + comb
+        }
+    }
+    export function onCombo(func: () => void){
+        while(true){
+            if(combin == combo){
+                func()
+            }
+        }
+    }
+    input.onButtonPressed(Button.A, function() {
+        combin=combin+'A'
+    })
+    input.onButtonPressed(Button.B, function () {
+        combin = combin + 'B'
+    })
 }
